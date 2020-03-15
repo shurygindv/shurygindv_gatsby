42eafacde0ea816fc6e34385017bb35cbfbc7f33
Daniil_Shurygin <Daniil_Shurygin@epam.com>
2020-03-15 20:48:33 +0400

    slider got smooth

--- src/ui/organisms/Slider/Slider.tsx
+++ src/ui/organisms/Slider/Slider.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import styled from 'styled-components';
+import { animated, useSpring } from 'react-spring';
 
 import { Image } from '~ui/atoms/Image';
 
@@ -18,17 +19,44 @@ const SliderItem = styled.article`
   flex-shrink: 0;
 `;
 
+const useAnimatedStyles = () => {
+  const [activeSlide, setActiveSlide] = React.useState(0);
+
+  const [spring, setSpring] = useSpring(() => ({
+    x: 0,
+  }));
+
+  const styles = {
+    transform: spring.x.interpolate(v => `translate3d(${v}%,0,0)`),
+  };
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setActiveSlide(v => v + 1);
+    }, 5000);
+  }, [activeSlide]);
+
+  React.useEffect(() => {
+    setSpring({
+      x: activeSlide * -100,
+    });
+  }, [activeSlide]);
+
+  return styles;
+};
+
 export const Slider: React.FC<Props> = props => {
   const data = useSliderImages();
+  const styles = useAnimatedStyles();
 
   return (
-    <SliderContainer>
+    <SliderContainer as={animated.section} style={styles}>
       {data.allFile.edges.map((image: any, i: number) => (
         <SliderItem key={i}>
           <Image
             fluid={image.node.childImageSharp.fluid}
             alt={image.node.base.split('.')[0]}
-            style={{'height': '300px'}}
+            style={{ height: '300px' }}
           />
         </SliderItem>
       ))}
